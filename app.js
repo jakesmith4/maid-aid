73ecc473ba499931f6b670be58df1f91b8605084
Jacob Smith <jakesmith.seo@gmail.com>
2024-02-22 15:37:35 -0600

    finished website

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 // Setup Date
-const date = (document.getElementById(
-  "date"
-).innerHTML = new Date().getFullYear());
+const date = (document.getElementById('date').innerHTML =
+  new Date().getFullYear());
 
 // Setup Nav
-const navBtn = document.getElementById("nav-btn");
-const navbar = document.getElementById("navbar");
-const navClose = document.getElementById("nav-close");
+const navBtn = document.getElementById('nav-btn');
+const navbar = document.getElementById('navbar');
+const navClose = document.getElementById('nav-close');
+
+// NOT USING THIS YET, MAY USE IN FUTURE
+// Setup Contact
+// const form = document.querySelector('.submit-btn');
+// const allFormControls = [...document.querySelectorAll('.form-control')];
 
 // Show Nav
-navBtn.addEventListener("click", () => {
-  navbar.classList.add("showNav");
+navBtn.addEventListener('click', () => {
+  navbar.classList.add('showNav');
 });
 
 // Close Nav
-navClose.addEventListener("click", () => {
-  navbar.classList.remove("showNav");
+navClose.addEventListener('click', () => {
+  navbar.classList.remove('showNav');
 });
